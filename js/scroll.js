b5456828782dc1aba83b1512e1f8acf2feb81c4c
Carolina-Vega-fs <carolinaangelesvega@hotmail.com>
2022-05-16 20:11:37 -0300

    Proyecto Finalizado

---
+++ js/scroll.js
@@ -0,0 +1,37 @@
+TweenMax.staggerFrom(".logo",1.6,{
+    delay:0.2,
+    opacity :0,
+    y: 20,
+    ease:Expo.easeInOut
+})
+TweenMax.staggerFrom(".menu",1.6,{
+    delay:0.2,
+    opacity :0,
+    y: 20,
+    ease:Expo.easeInOut
+})
+TweenMax.staggerFrom(".right-nav",1.6,{
+    delay:0.2,
+    opacity :0,
+    y: 20,
+    ease:Expo.easeInOut
+})
+
+TweenMax.staggerFrom("#search-banner",3,{
+    delay:0.7,
+    opacity :0,
+    y: 70,
+    ease:Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".search-banner-text",3,{
+    delay:2,
+    opacity :0,
+    y: 70,
+    ease:Expo.easeInOut
+})
+
+
+
+
+
